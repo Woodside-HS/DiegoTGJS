313e6b33108637ee315c10fb1b2b0d3b4e61835f
Eric Ettlin <eettlin@seq.org>
2017-06-02 12:52:58 -0700

    Diego First Commit

---
+++ TGJS_Diego/resources/images/spritesheets/buttons.js
@@ -0,0 +1,51 @@
+buttons = {"frames": {
+
+"B10000":
+{
+	"frame": {"x":0,"y":0,"w":78,"h":100},
+	"rotated": false,
+	"trimmed": false,
+	"spriteSourceSize": {"x":0,"y":0,"w":78,"h":100},
+	"sourceSize": {"w":78,"h":100}
+},
+"B20000":
+{
+	"frame": {"x":78,"y":0,"w":78,"h":100},
+	"rotated": false,
+	"trimmed": false,
+	"spriteSourceSize": {"x":0,"y":0,"w":78,"h":100},
+	"sourceSize": {"w":78,"h":100}
+},
+"B30000":
+{
+	"frame": {"x":156,"y":0,"w":78,"h":100},
+	"rotated": false,
+	"trimmed": false,
+	"spriteSourceSize": {"x":0,"y":0,"w":78,"h":100},
+	"sourceSize": {"w":78,"h":100}
+},
+"B40000":
+{
+	"frame": {"x":0,"y":100,"w":78,"h":100},
+	"rotated": false,
+	"trimmed": false,
+	"spriteSourceSize": {"x":0,"y":0,"w":78,"h":100},
+	"sourceSize": {"w":78,"h":100}
+},
+"B50000":
+{
+	"frame": {"x":78,"y":100,"w":78,"h":100},
+	"rotated": false,
+	"trimmed": false,
+	"spriteSourceSize": {"x":0,"y":0,"w":78,"h":100},
+	"sourceSize": {"w":78,"h":100}
+}},
+"meta": {
+	"app": "Adobe Flash CS6",
+	"version": "12.0.0.481",
+	"image": "sprites buttons.png",
+	"format": "RGBA8888",
+	"size": {"w":256,"h":256},
+	"scale": "1"
+}
+}
